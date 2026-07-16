cda11ff1f99b587aaa1fb7e98fa23edc4497ab0a
kent <100090733+cgyinthehouse@users.noreply.github.com>
2026-07-16 21:44:13 +0800

    make "grr" keymap to use lsp references of Snacks picker

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -91,6 +91,10 @@ return {
             return client:supports_method "textDocument/semanticTokens/full" and vim.lsp.semantic_tokens ~= nil
           end,
         },
+        grr = {
+          function() require("snacks.picker").lsp_references() end,
+          desc = "Search references",
+        },
       },
     },
     -- A custom `on_attach` function to be run after the default `on_attach` function
